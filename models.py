b64f42bd5e9d4094bdc173b9cc288d2608ef50a5
Ahmed-Mokhtar-Abdelsattar <ahmed.mokhtar.abdelsattar@gmail.com>
2020-07-25 06:14:11 +0200

    Change network architecture

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-## TODO: define the convolutional neural network architecture
+## Defining the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
+#from torch.autograd import Variable # from abojja9 (GitHub)
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,11 +20,33 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        #self.conv1 = nn.Conv2d(1, 32, 5)###########################AMA
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        ####################################################### AMA #######################################################
         
+         # Covolutional Layers
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv5 = nn.Conv2d(256, 512, 1)
+
+        # Maxpooling Layer
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Fully Connected Layers
+        #self.fc1 = nn.Linear(in_features = 512*6*6, out_features = 1024) # The number of input gained by "print("Flatten size: ", x.shape)" in below
+        #self.fc2 = nn.Linear(in_features = 1024,    out_features = 512)
+        #self.fc3 = nn.Linear(in_features = 512,    out_features = 136) # the output 136 in order to having 2 for each of the 68 keypoint (x, y) pairs
+
+        self.fc1 = nn.Linear(512*6*6, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 68*2)
+        
+        # Dropout
+        self.dropout = nn.Dropout(p=0.25)
 
         
     def forward(self, x):
@@ -34,4 +56,25 @@ class Net(nn.Module):
         
         
         # a modified x, having gone through all the layers of your model, should be returned
+        
+        ####################################################### AMA #######################################################
+
+        
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.conv5(x)))
+
+        # Flattening the layer
+        x = x.view(x.size(0), -1)
+        #print("Flatten size: ", x.shape)
+
+             # linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        
         return x
